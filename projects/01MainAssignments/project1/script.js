d021102b9db6ded97a6a0e5071fd854cd06c4c2b
Fabio <57631740+fblzz@users.noreply.github.com>
2025-11-25 09:48:59 +0100

    Refactor code structure for improved readability and maintainability
    Added snake animation with advanced graphics
    
    Extended mousePressed interactions

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -3,21 +3,26 @@ let
 	montserrat,
 	prism,
 	txt,
+	graphic,
 	x = 0,
 	y = 0,
 	size = 20,
 	gap = 20,
 	angle = 0,
-	offset = 0,
+	offset,
 	hov_col,
 	waveSpeed = 0.04,
 	increment;
+	nth = 10;
+i_minVal = -50;
+i_maxVal = 50;
 
 function preload() {
 	// load the font
 	jost_italic = loadFont('jost-italic.otf');
 	montserrat = loadFont('Montserrat-Black.ttf');
 	prism = loadModel('prism.obj', true);
+	graphic = loadImage('graphic.png');
 	txt = loadImage("https://media.istockphoto.com/id/1427442928/photo/blue-glass-texture.jpg?s=612x612&w=0&k=20&c=59naaey41ZhS-FDFj5f8Fdvw6hES6D820rfjykQwsas=")
 }
 
@@ -25,6 +30,7 @@ function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	textFont(jost_italic);
 	orbitControl();
+	increment = TWO_PI / 180;
 
 	/*
 	textTexture = createGraphics(500, 500);
@@ -70,7 +76,6 @@ function draw() {
 
 		// wave parameters (y-axis)
 		const waveAmp = 4; // vertical amplitude in pixels
-		//let waveSpeed = 0.04; // how fast the wave moves
 		const wavePhaseRow = row * 0.35; // phase offset per row
 
 		for (let i = 0; i < lineWords.length; i++) {
@@ -143,13 +148,70 @@ function draw() {
 	pop();
 	*/
 
+
+	// snake bottom
+	for (let i = 0 - width; i < width; i += 12) {
+		let offset = i / 20;
+		let y = sinMovment(angle, offset, 0, 50);
+		fill(255, 255, 255, 100);
+		if (i % nth == 0) {
+			fill(120, 197, 239, 255);
+		}else {
+			fill(255, 255, 255, 100);
+		}
+		circle(i, y + (height / 2) - 150, 10);
+		circle(i, y - (height / 2) + 100, 10);
+	}
+
+	for (let i = 0 - width; i < width; i += 110) {
+		let offset = i / 200;
+		let y = sinMovment(angle, offset, i_minVal, i_maxVal);
+		texture(graphic);
+		circle(-i, y, 100);
+	}
+
+	// increment angle
+	angle += increment;
+
 }
+
+
+function sinMovment(angle, offset, minVal, maxVal) {
+	let factor = sin(angle + offset);
+	// Change the sinus output range (-1, 1) to another range (minVal, maxVal)
+	let sinMovementVal = map(factor, -1, 1, minVal, maxVal);
+	return sinMovementVal;
+}
+
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 }
 
 function mousePressed() {
-	waveSpeed += 0.02;
-	
+
+	if (waveSpeed < 0.16) {
+		waveSpeed += 0.02;
+	} else {
+		waveSpeed = 0.04;
+	}
+
+	if (i_maxVal < 200) {
+		i_minVal -= 25;
+		i_maxVal += 25;
+	} else {
+		i_minVal = -50;
+		i_maxVal = 50;
+	}
+
+	if (nth < 20) {
+		nth += 3;
+	} else {
+		nth = 10;
+	}
+
+	console.log(waveSpeed);
+	console.log(i_minVal, i_maxVal);
+	console.log(nth);
+
 }
